367d9e7862ea5995b8695881105f2cd61daa9e59
antciccone <aciccone@bu.edu>
2017-01-06 09:25:48 -0700

    set up user can search for best buy stores

--- spec/features/integration/display_items_spec.rb
+++ spec/features/integration/display_items_spec.rb
@@ -5,7 +5,6 @@ describe 'IntegrationTest' do
 
     visit '/'
     assert_equal 200, page.status_code
-
     within('h1') do
       assert page.has_content?('Items'), "page should have an Items heading"
     end
---
+++ spec/features/user/a_user_see_stores_with_in_25_miles_of_location_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe 'As a user' do
+  context 'when i enter a zipcode' do
+    scenario 'I can see stores within 25 miles' do
+
+      visit '/'
+      click 'search'
+      fill_in :zip, with: '80202'
+
+      expect(page).to have_content("16 Total Stores")
+      expect(page).to have_content("city")
+    end
+  end
+end
+
+
+
+# As a user
+# When I visit "/"
+# And I fill in a search box with "80202" and click "search"
+# Then my current path should be "/search" (ignoring params)
+# And I should see stores within 25 miles of 80202
+# And I should see a message that says "16 Total Stores"
+# And I should see exactly 15 results
+# And I should see the long name, city, distance, phone number and store type for each of the 15 results
