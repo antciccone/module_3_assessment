d5306bbd1b0586f8bc07055804029512a854b00d
antciccone <aciccone@bu.edu>
2017-01-06 11:28:34 -0700

    pass test for delete method for api

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -42,22 +42,35 @@ describe 'all items endpoint' do
   end
 
   context 'api/vi/items' do
-      it 'creates an item' do
+    it 'creates an item' do
       item = Item.create(name: 'Anthony', description: "turing student", image_url: "google.com/images" )
       item_params = { name: 'john', description: 'test', image_url: 'googe.com/imagesss' }
 
       post "/api/v1/items", item: item_params
 
       items = JSON.parse(response.body)
-      require "pry"; binding.pry
 
       expect(response).to be_success
-      expect(response.status).to eq(200)
+      expect(response.status).to eq(201)
+
+      expect(items.count).to eq(2)
       expect(item["name"]).to eq('Anthony')
       expect(item["description"]).to eq("turing student")
       expect(item["image_url"]).to eq("google.com/images")
-      expect(item["created_at"]).to eq(nil)
-      expect(item["updated_at"]).to eq(nil)
+      expect(items.first["created_at"]).to eq(nil)
+      expect(items.first["updated_at"]).to eq(nil)
+    end
+  end
+
+  context 'api/vi/items' do
+    it 'deletes an item' do
+      item1 = Item.create(name: 'Anthony', description: "turing student", image_url: "google.com/images" )
+      item2 = Item.create(name: 'john', description: "turing student", image_url: "google.com/images" )
+
+      delete "/api/v1/items/#{item1.id}"
+
+      expect(response).to be_success
+      expect(response.status).to eq(201)
     end
   end
 end
